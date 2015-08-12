71402d2f301de45e879c1d896a3a00594abaa6db
Daniel <dan.t.ching@gmail.com>
2015-08-11 22:09:09 -0700

    winner is you mail

--- management/commands/check_active.py
+++ management/commands/check_active.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 
 from silent_auction.models import Auction
+from silent_auction import mailbot
 
 class Command(BaseCommand):
 
@@ -13,4 +14,11 @@ class Command(BaseCommand):
             if timezone.now() > x.end_time:
                 x.active = False
                 x.save()
-
+                bids = x.bid_set.order_by('-amount')
+                if len(bids) > 0:
+                    winner = bids[0]
+                    message = "Congratulations! You won the auction for " + unicode(x.item_name) + " with a bid of " + unicode(winner.amount) + ". Make your donation at [website here] as soon as possible."
+                    subject = "Auction for " + unicode(x.item_name) + ":A winner is you!"
+                    mail = mailbot.pack_MIME(message, winner.email, subject)
+                    mailbot.send_mail(mail, winner.email)
+                    print mail
